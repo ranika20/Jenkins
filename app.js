5a46aafdb478513a03e6da83c89918d65b7fc8ec
ranik20 <ranik20@gmail.com>
2026-08-16 20:06:43 +0300

    fix port in apps

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.get("/", (req, res) => {
   `);
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
